bcaf40fb43ac1b9e5cfb06374ee6d8c76515e7ff
Calebbii <biicaleb316@gmail.com>
2024-02-20 00:06:55 +0300

    updates the acknowledgement section

--- Solutions/remove_duplicates.py
+++
@@ -1,9 +0,0 @@
-def removeduplicate(numbers):
-    sorted =[]
-    for n in numbers:
-        if n not in sorted:
-            sorted.append(n)
-        
-    return f'Output: k={len(sorted)}, nums ={sorted}'
-
-print (removeduplicate([1,1,1,6,1,1,2,3,1,1]))
