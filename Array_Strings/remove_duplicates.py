f628366f238bde50bc3048bee11f7921f49483c6
Calebbii <biicaleb316@gmail.com>
2024-02-20 12:33:28 +0300

    clasifies the solutions to the correct folder structure

---
+++ Array_Strings/remove_duplicates.py
@@ -0,0 +1,11 @@
+def removeduplicate(nums):
+    if not nums:
+            return 0
+    
+    k = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            nums[k] = nums[i]
+            k += 1
+    
+    return k
--- Solutions/remove_element.py
+++
@@ -1,8 +0,0 @@
-def remove(nums, n):
-    for num in nums:
-        if num == n:
-            nums.remove(num)
-    return  f'{nums}, k={len(nums)}'
-
-test3= [1,3,4,5,6,2,1,3]
-print(remove([1,3,4,5,6,2,1,3], 3)) 
