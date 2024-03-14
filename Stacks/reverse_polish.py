f1a0f222675b7ecb23f323dadc37a32e20d6032f
Calebbii <biicaleb316@gmail.com>
2024-03-14 23:47:19 +0300

    adds a function that evaluates arithmetic expression in reverse polish notation

---
+++ Stacks/reverse_polish.py
@@ -0,0 +1,20 @@
+def evalRPN(tokens):
+    stack = []
+    operators = {'+', '-', '*', '/'}
+    
+    for token in tokens:
+        if token in operators:
+            b = stack.pop()
+            a = stack.pop()
+            if token == '+':
+                stack.append(a + b)
+            elif token == '-':
+                stack.append(a - b)
+            elif token == '*':
+                stack.append(a * b)
+            else:
+                stack.append(int(a / b))
+        else:
+            stack.append(int(token))
+    
+    return stack[0]
