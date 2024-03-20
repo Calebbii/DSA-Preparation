4fa9e883ebb44f83ad19cda9c556318b8232870e
Calebbii <biicaleb316@gmail.com>
2024-03-20 23:20:18 +0300

    adds linked list cycle

---
+++ Linked_List/linked_list_cycle.js
@@ -0,0 +1,18 @@
+function hasCycle(head) {
+    if (!head || !head.next) {
+        return false;
+    }
+    
+    let slow = head;
+    let fast = head.next;
+    
+    while (slow !== fast) {
+        if (!fast || !fast.next) {
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+    return true;
+}
